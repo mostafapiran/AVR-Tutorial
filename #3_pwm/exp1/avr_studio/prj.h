6fd0695688fcacfcf9069b620c0d5282e59384ce
Mostafa Piran <piran.m313@gmail.com>
2023-11-13 04:13:23 +0330

    Create prj.h

---
+++ #3_pwm/exp1/avr_studio/prj.h
@@ -0,0 +1,24 @@
+#ifndef _PRJ_H
+#define _PRJ_H
+
+#define F_CPU 7372800UL
+#include <avr/io.h> 
+#include <avr/sfr_defs.h> 
+#include <util/delay.h> 
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/twi.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h> // define uint32_t
+
+#include "pin_config.h" 
+#include "key.h"
+#include "lcm4b.h" 
+#include "uart.h"
+#include "main.h" 
+#include "led.h"
+ 
+ 
+#endif
