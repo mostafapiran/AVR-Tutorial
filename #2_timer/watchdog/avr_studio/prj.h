445e81d026be28b500fe8679c2ce14b6fd1be5df
Mostafa Piran <piran.m313@gmail.com>
2023-11-13 04:12:37 +0330

    Create prj.h

---
+++ #2_timer/watchdog/avr_studio/prj.h
@@ -0,0 +1,16 @@
+#ifndef _PRJ_H
+#define _PRJ_H
+
+#define F_CPU 7372800UL
+#include <avr/io.h> 
+#include <avr/sfr_defs.h> 
+#include <util/delay.h> 
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+#include "pin_config.h"
+#include "led.h"
+#include "key.h"
+#include "timer.h"
+#include "main.h" 
+#endif
