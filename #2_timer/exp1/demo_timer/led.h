4728e41e52f03f41b9015cd5e72fd3521b6b4ad8
Mostafa Piran <piran.m313@gmail.com>
2023-11-13 04:11:45 +0330

    Create led.h

---
+++ #2_timer/exp1/demo_timer/led.h
@@ -0,0 +1,17 @@
+#ifndef _LED_H
+#define _LED_H
+
+// 0 - active low, 1 - active high
+#define LED1_ACTIVE_LOW    0
+
+#if (LED1_ACTIVE_LOW ==0) 
+    #define Led1_on()     Led1_PORT &= ~_BV(Led1_BIT) 
+    #define Led1_off()    Led1_PORT |=  _BV(Led1_BIT) 
+#else 
+    #define Led1_on()     Led1_PORT |=  _BV(Led1_BIT) 
+    #define Led1_off()    Led1_PORT &= ~_BV(Led1_BIT) 
+#endif
+
+void Led_blink();
+
+#endif
