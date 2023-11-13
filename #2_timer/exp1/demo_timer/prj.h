1d2565b0497e7bbb9be8005085ffd4db36d86dc2
Mostafa Piran <piran.m313@gmail.com>
2023-11-13 04:11:52 +0330

    Create prj.h

---
+++ #2_timer/exp1/demo_timer/prj.h
@@ -0,0 +1,14 @@
+#ifndef _PRJ_H
+#define _PRJ_H
+
+#define F_CPU 7372800UL
+#include <avr/io.h> 
+#include <avr/sfr_defs.h> 
+#include <util/delay.h> 
+#include <avr/interrupt.h>
+
+#include "pin_config.h"
+#include "led.h"
+#include "timer.h"
+#include "main.h" 
+#endif
