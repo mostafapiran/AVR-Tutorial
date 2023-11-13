0741dcb8a481038128b576b513197d27dfabef77
Mostafa Piran <piran.m313@gmail.com>
2023-11-13 04:16:33 +0330

    Create prj.h

---
+++ #6_interrupt/exp1/AVR_studio/prj.h
@@ -0,0 +1,15 @@
+#ifndef _PRJ_H
+#define _PRJ_H
+
+#define F_CPU 4000000UL
+#include <avr/io.h> 
+#include <avr/sfr_defs.h> 
+#include <util/delay.h> 
+#include <avr/interrupt.h>
+
+#include "pin_config.h"
+#include "key.h"
+#include "segment.h"
+#include "interrupt.h"
+#include "main.h" 
+#endif
